6b8b5eee3c6076991ccb121c24e02d34fd9f2646
joycenerd <zchin31415@gmail.com>
2021-03-24 19:21:18 +0800

    calculate likelihood and prior

--- HW2/naive_bayes_classifier.py
+++ HW2/naive_bayes_classifier.py
@@ -44,11 +44,37 @@ if __name__=="__main__":
     # Discrete case
     if toggle=="0":
         # gray level [0-255], convert to 32 bins
-        bin=np.zeros((num_of_train,rows,cols))
+        bin=np.zeros((num_of_train,rows,cols),dtype=int)
         for i in range(num_of_train):
             for j in range(rows):
                 for k in range(cols):
                     bin[i][j][k]=int(train_image[i][j][k]/8)
 
         # Calculate likelihood
+        bin=bin.reshape(num_of_train,-1) # [60000,784]
+        likelihood=np.empty((10,rows*cols,32))  # [10,784,32], initialize with some small number 1e-8
+        likelihood.fill(1e-8)
+        likelihood_sum=np.zeros((10,rows*cols)) # [10,784]
+
+        for i in range(num_of_train):
+            for j in range(rows*cols):
+                label=train_label[i]
+                value=bin[i][j]
+                likelihood[label][j][value]+=1
+        for i in range(10):
+            for j in range(rows*cols):
+                for k in range(32):
+                    likelihood_sum[i][j]+=likelihood[i][j][k]
+        for i in range(10):
+            for j in range(rows*cols):
+                for k in range(32):
+                    likelihood[i][j][k]/=likelihood_sum[i][j]
+
+        # Calculate prior
+        prior=np.zeros(10,dtype=np.double)
+        for label in train_label:
+            prior[label]+=1
+        prior/=train_label.shape[0]
+
+        # Calculate posterior
 
