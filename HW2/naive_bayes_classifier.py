47e1d4e29327c6739b3c468e33bb860ac05dbcdf
joycenerd <zchin31415@gmail.com>
2021-03-27 17:06:30 +0800

    format code

--- HW2/naive_bayes_classifier.py
+++ HW2/naive_bayes_classifier.py
@@ -47,7 +47,7 @@ def get_prior(data_label, num_of_data):
     return class_cnt, prior
 
 
-def print_posterior_calc_error(test_label,all_posterior):
+def print_posterior_calc_error(test_label, all_posterior):
     err = 0.0
     for i, posterior in enumerate(all_posterior):
         print("Posterior (in log scale):")
@@ -55,16 +55,16 @@ def print_posterior_calc_error(test_label,all_posterior):
             print('{}: {}'.format(classes, class_prob))
         pred = np.argmin(posterior)
         label = test_label[i]
-        print("Prediction: {}, Ans: {}".format(pred,label))
+        print("Prediction: {}, Ans: {}".format(pred, label))
         if pred != label:
             err += 1
         print("")
     return err
 
 
-def print_err(err,num_of_data):
-    err_rate=err/num_of_data
-    print("Error rate: "+str(err_rate))
+def print_err(err, num_of_data):
+    err_rate = err / num_of_data
+    print("Error rate: " + str(err_rate))
 
 
 if __name__ == "__main__":
@@ -93,7 +93,6 @@ if __name__ == "__main__":
         # gray level [0-255], convert to 32 bin
         train_bin = tally_freq(num_of_train, rows, cols, train_image)
 
-
         # Calculate likelihood
         train_bin = train_bin.reshape(num_of_train, -1)  # [60000,784]
         likelihood = np.zeros((10, rows * cols, 32), dtype=np.double)  # [10,784,32]
@@ -113,7 +112,6 @@ if __name__ == "__main__":
                     else:
                         likelihood[cls][px_idx][bin] = 1e-8
 
-
         # Calculate posterior
         all_posterior = []
         test_bin = tally_freq(num_of_test, rows, cols, test_image)
@@ -129,10 +127,8 @@ if __name__ == "__main__":
             posterior /= np.sum(posterior)
             all_posterior.append(posterior)
 
-
         # Print posterior
-        err=print_posterior_calc_error(test_label,all_posterior)
-
+        err = print_posterior_calc_error(test_label, all_posterior)
 
         # print imagination number --> from likelihood
         likelihood = likelihood.reshape(10, rows, cols, -1)
@@ -152,65 +148,62 @@ if __name__ == "__main__":
                 print('')
             print('')
 
-
         # print error rate
-        print_err(err,num_of_test)
+        print_err(err, num_of_test)
 
     # Continuous mode
     if toggle == "1":
         # Calculate prior
         class_cnt, prior = get_prior(train_label, num_of_train)
 
-
         # Calculate likelihood
         train_image = train_image.reshape((num_of_train, -1))  # [60000,784]
-        mean = np.zeros((10, rows * cols), dtype=np.double) # [10,784]
-        variance = np.zeros((10, rows * cols), dtype=np.double) # [10,784]
-        sum_of_train = np.zeros((10, rows * cols), dtype=np.double) # [10,784]
+        mean = np.zeros((10, rows * cols), dtype=np.double)  # [10,784]
+        variance = np.zeros((10, rows * cols), dtype=np.double)  # [10,784]
+        sum_of_train = np.zeros((10, rows * cols), dtype=np.double)  # [10,784]
 
         # get mean, variance
         for i in range(num_of_train):
             for j in range(rows * cols):
                 label = train_label[i]
                 sum_of_train[label][j] += train_image[i][j]
-                variance[label][j]+=train_image[i][j]**2
+                variance[label][j] += train_image[i][j] ** 2
 
         for i in range(10):
             for j in range(rows * cols):
                 mean[i][j] = sum_of_train[i][j] / class_cnt[i]
-                variance[i][j]=variance[i][j]/class_cnt[i]-(mean[i][j]**2)+1000  # var=sum(x)^2/n-mean^2
-
+                variance[i][j] = variance[i][j] / class_cnt[i] - (mean[i][j] ** 2) + 1000  # var=sum(x)^2/n-mean^2
 
         # Calculate posterior
-        test_image=test_image.reshape(num_of_test,-1) # [10000,784]
+        test_image = test_image.reshape(num_of_test, -1)  # [10000,784]
         posterior_list = []
 
         for n, px_list in enumerate(test_image):
-            posterior = np.zeros(10,dtype=np.double)
+            posterior = np.zeros(10, dtype=np.double)
             for c in range(10):
                 posterior[c] += math.log(prior[c])
                 for px_idx, px in enumerate(px_list):
                     if variance[c][px_idx] == 0.0:  # prevent from divide by 0
                         variance[c][px_idx] = 1e-8
                     g = math.log(1.0 / math.sqrt(2.0 * math.pi * variance[c][px_idx])) - (
-                                (px - mean[c][px_idx]) ** 2 / (2.0 * variance[c][px_idx]))
+                            (px - mean[c][px_idx]) ** 2 / (2.0 * variance[c][px_idx]))
                     posterior[c] += g
             posterior = posterior / np.sum(posterior)  # marginalize
             posterior_list.append(posterior)
 
-        err=print_posterior_calc_error(test_label,posterior_list)
+        err = print_posterior_calc_error(test_label, posterior_list)
 
         # print imagination number
-        mean=mean.reshape((10,rows,cols))
+        mean = mean.reshape((10, rows, cols))
         for i in range(10):
-            print(str(i)+":")
+            print(str(i) + ":")
             for j in range(rows):
                 for k in range(cols):
-                    if mean[i][j][k]<128:
-                        print("0",end=" ")
+                    if mean[i][j][k] < 128:
+                        print("0", end=" ")
                     else:
-                        print("1",end=" ")
+                        print("1", end=" ")
                 print("")
             print("")
 
-        print_err(err,num_of_test)
+        print_err(err, num_of_test)
