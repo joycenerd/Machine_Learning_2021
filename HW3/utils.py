c190379e6a90587301ff67c3ce4380f1b2be0b66
joycenerd <zchin31415@gmail.com>
2021-04-08 19:20:02 +0800

    modified box muller

--- HW3/utils.py
+++ HW3/utils.py
@@ -1,15 +1,24 @@
 import numpy as np
 
 
-def Gaussian_data_gen(mean,variance):
+def Gaussian_data_gen(mean, variance):
     """
     Gaussian_data_gen: generate standard Gaussian data with given mean and variance via Box Muller method
     :param mean: Gaussian mean
     :param variance: Gaussian variance
     :return: number generate from standard Gaussian
     """
-    U = np.random.uniform(0,1)
-    V = np.random.uniform(0,1)
-    X=np.sqrt(-2*np.log(U))*np.cos(2*np.pi*V)
-    rand_num = mean + np.sqrt(variance) * X
-    return rand_num
+    # box-muller
+    u = np.random.uniform(0.0, 1.0)
+    v = np.random.uniform(0.0, 1.0)
+    z = np.sqrt(-2.0 * np.log(u)) * np.sin(2 * np.pi * v)
+    return mean + np.sqrt(variance) * z
+
+
+def input_poly_basis():
+    n = int(input("Input n: "))
+    a = float(input("Input a: "))
+    w_str = input("Input w: ")
+    w = list(map(float, w_str.split()))
+    w = np.array(w).reshape(n, -1)  # [n,1]
+    return n, a, w
