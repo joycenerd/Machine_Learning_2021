408f152a25e68298630f45cfe641b341e689aa17
joycenerd <zchin31415@gmail.com>
2021-04-08 19:18:25 +0800

    polynomial data generator as funtion

--- HW3/random_data_generater.py
+++ HW3/random_data_generater.py
@@ -1,31 +1,29 @@
 import numpy as np
-from utils import Gaussian_data_gen
+from utils import Gaussian_data_gen, input_poly_basis
 
 
-if __name__=='__main__':
+def poly_data_gen(n, a, w):
+    x = np.random.uniform(-1.0, 1.0)
+    X = np.array([(x ** deg) for deg in range(n)]).reshape(-1)
+    W = np.array(w).reshape(-1)
+    e = Gaussian_data_gen(0, a)
+    return x, np.dot(np.transpose(W), X) + e
+
+
+if __name__ == '__main__':
 
     while True:
-        toggle=input('Univariate Gaussian (0) or Polynomial basis linear mode (1): ')
+        toggle = input('Univariate Gaussian (0) or Polynomial basis linear mode (1): ')
 
         # Univariate Gaussian data generator
-        if toggle=='0':
-            mean=float(input("Input mean: "))
-            variance=float(input("Input variance: "))
-            rand_num=Gaussian_data_gen(mean,variance)
+        if toggle == '0':
+            mean = float(input("Input mean: "))
+            variance = float(input("Input variance: "))
+            rand_num = Gaussian_data_gen(mean, variance)
             print(rand_num)
 
         # Polynomial basis linear model data generator
-        elif toggle=='1':
-            n=int(input("Input n: "))
-            a=float(input("Input a: "))
-            w_str=input("Input w: ")
-            w=list(map(float,w_str.split()))
-            w=np.array(w).reshape(n,-1) # [n,1]
-            x=np.random.uniform(-1.0,1.0)
-            X=np.array([x**i for i in range(n)]).reshape(1,n) # [1,n]
-            e=Gaussian_data_gen(0,a)
-            y=X@w+e
-            y=y.squeeze().squeeze()
+        elif toggle == '1':
+            n, a, w = input_poly_basis()
+            x, y = poly_data_gen(n, a, w)
             print(y)
-
-
