058485afb7bdadab74ff5ceb0dc9931911fad573
joycenerd <joycenerd.cs09@nycu.edu.tw>
2021-06-11 21:29:26 +0800

    finish sne

--- HW7/t-SNE.py
+++ HW7/t-SNE.py
@@ -12,18 +12,27 @@
 #  Created by Laurens van der Maaten on 20-12-08.
 #  Copyright (c) 2008 Tilburg University. All rights reserved.
 
+from PIL import Image
 from scipy.spatial.distance import cdist
+from numpy.linalg import pinv, eig, norm
+import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
 
 import argparse
 import pylab
+import os
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--max_iter", type=int, default=500,
+parser.add_argument("--max_iter", type=int, default=1000,
                     help="Maximum iteration")
 parser.add_argument("--method", type=str, default="t-sne",
                     help="Which sne method: [t-sne, s-sne]")
+parser.add_argument("--dim-reduction", type=str, default="pca",
+                    help="Which dimension reduction method: [pca,lda]")
+parser.add_argument("--perplexity", type=int, default=50,
+                    help="perplexity for sne: [30,50,70]")
 args = parser.parse_args()
 
 
@@ -50,6 +59,7 @@ def x2p(X=np.array([]), tol=1e-5, perplexity=30.0):
         Performs a binary search to get P-values in such a way that each
         conditional Gaussian has the same perplexity.
     """
+    perplexity = args.perplexity
 
     # Initialize some variables
     print("Computing pairwise distances...")
@@ -119,13 +129,42 @@ def pca(X=np.array([]), no_dims=50):
     return Y
 
 
-def sne(X=np.array([]), no_dims=2, initial_dims=50, perplexity=30.0):
+def lda(X, label, no_dims=50):
+    (n, d) = X.shape
+    label = np.asarray(label)
+    c = np.unique(label)
+    mu = np.mean(X, axis=0)
+    S_w = np.zeros((d, d), dtype=np.float64)
+    S_b = np.zeros((d, d), dtype=np.float64)
+
+    # Sw=(xi-mj)*(xi-mj)^T
+    # Sb=nj*(mj-m)*(mj-m)^T
+    for i in c:
+        X_i = X[np.where(label == i)[0], :]
+        mu_i = np.mean(X_i, axis=0)
+        S_w += (X_i - mu_i).T @ (X_i - mu_i)
+        S_b += X_i.shape[0] * ((mu_i - mu).T @ (mu_i - mu))
+
+    # get eigenvalues and eigenvectors
+    S = pinv(S_w) @ S_b
+    eigen_val, eigen_vec = eig(S)
+    for i in range(eigen_vec.shape[1]):
+        eigen_vec[:, i] = eigen_vec[:, i] / norm(eigen_vec[:, i])
+
+    W = eigen_vec[:, :no_dims].real
+    Y = X@W
+    return Y
+
+
+def sne(X, labels, no_dims=2, initial_dims=50, perplexity=30.0):
     """
         Runs t-SNE on the dataset in the NxD array X to reduce its
         dimensionality to no_dims dimensions. The syntaxis of the function is
         `Y = tsne.tsne(X, no_dims, perplexity), where X is an NxD NumPy array.
     """
     method = args.method
+    dim_reduction = args.dim_reduction
+    perplexity = args.perplexity
 
     # Check inputs
     if isinstance(no_dims, float):
@@ -136,7 +175,10 @@ def sne(X=np.array([]), no_dims=2, initial_dims=50, perplexity=30.0):
         return -1
 
     # Initialize variables
-    X = pca(X, initial_dims).real
+    if dim_reduction == "pca":
+        X = pca(X, initial_dims).real
+    elif dim_reduction == "lda":
+        X = lda(X, labels)
     (n, d) = X.shape
     max_iter = args.max_iter
     initial_momentum = 0.5
@@ -154,6 +196,9 @@ def sne(X=np.array([]), no_dims=2, initial_dims=50, perplexity=30.0):
     P = P / np.sum(P)
     P = P * 4.									# early exaggeration
     P = np.maximum(P, 1e-12)
+
+    C_list = []
+    Y_list = []
 
     # Run iterations
     for iter in range(max_iter):
@@ -175,8 +220,12 @@ def sne(X=np.array([]), no_dims=2, initial_dims=50, perplexity=30.0):
         # Compute gradient
         PQ = P - Q
         for i in range(n):
-            dY[i, :] = np.sum(np.tile(PQ[:, i] * num[:, i],
-                              (no_dims, 1)).T * (Y[i, :] - Y), 0)
+            if method == "t-sne":
+                dY[i, :] = np.sum(np.tile(PQ[:, i] * num[:, i],
+                                          (no_dims, 1)).T * (Y[i, :] - Y), 0)
+            elif method == "s-sne":
+                dY[i, :] = np.sum(
+                    (np.tile(PQ[:, i], (no_dims, 1)).T) * (Y[i, :] - Y), axis=0)
 
         # Perform the update
         if iter < 20:
@@ -189,26 +238,98 @@ def sne(X=np.array([]), no_dims=2, initial_dims=50, perplexity=30.0):
         iY = momentum * iY - eta * (gains * dY)
         Y = Y + iY
         Y = Y - np.tile(np.mean(Y, 0), (n, 1))
+        Y_list.append(Y)
 
         # Compute current value of cost function
         if (iter + 1) % 10 == 0:
             C = np.sum(P * np.log(P / Q))
             print("Iteration %d: error is %f" % (iter + 1, C))
+            C_list.append(C)
 
         # Stop lying about P-values
         if iter == 100:
             P = P / 4.
 
     # Return solution
-    return Y
+    return Y_list, P, Q, C_list
+
+
+def plot_result(Y):
+    method = args.method
+    perplexity = args.perplexity
+    dim_reduction = args.dim_reduction
+    max_iter = args.max_iter
+
+    # save plots
+    save_dir = f"{SAVE_PATH}{method}_{perplexity}_{dim_reduction}/"
+    if not os.path.isdir(save_dir):
+        os.mkdir(save_dir)
+
+    for idx, y in enumerate(Y):
+        if (idx+1) % 10 == 0:
+            plt.clf()
+            plt.cla()
+            plt.close()
+            scatter = plt.scatter(y[:, 0], y[:, 1], 5, labels)
+            plt.legend(*scatter.legend_elements(),
+                       loc='lower left', title='Digit')
+            plt.title(
+                f'{method}, perplexity: {perplexity}, dim_reduction: {dim_reduction} iteration: {idx}')
+            plt.tight_layout()
+            plt.savefig(f'{save_dir}{idx}.png')
+
+    # make gif
+    vid_dir = SAVE_PATH+"video/"
+    imgs = []
+    for i in range(max_iter):
+        if (i+1) % 10 == 0:
+            image_name = f"{save_dir}{i}.png"
+            imgs.append(Image.open(image_name))
+    imgs[0].save(f"{vid_dir}{method}_{perplexity}_{dim_reduction}.gif", format='GIF', append_images=imgs[1:], loop=0,
+                 save_all=True, duration=300)
+
+
+def plot_similarity(P, Q):
+    method = args.method
+    perplexity = args.perplexity
+    dim_reduction = args.dim_reduction
+
+    # x = P or Q
+    plt.clf()
+    x = np.log(P)
+    index = np.argsort(labels)
+    x = x[index, :][:, index]
+    img = plt.imshow(x, cmap='viridis', vmin=np.min(x), vmax=np.max(x))
+    plt.colorbar(img)
+    plt.savefig(f'{SAVE_PATH}{method}_{perplexity}_{dim_reduction}_High-D.png')
+
+    plt.clf()
+    x = np.log(Q)
+    index = np.argsort(labels)
+    x = x[index, :][:, index]
+    img = plt.imshow(x, cmap='viridis', vmin=np.min(x), vmax=np.max(x))
+    plt.colorbar(img)
+    plt.savefig(f'{SAVE_PATH}{method}_{perplexity}_{dim_reduction}_Low-D.png')
 
 
 if __name__ == "__main__":
+    method = args.method
+    max_iter = args.max_iter
+    dim_reduction = args.dim_reduction
+
     print("Run Y = tsne.tsne(X, no_dims, perplexity) to perform t-SNE on your dataset.")
     print("Running example on 2,500 MNIST digits...")
     X = np.loadtxt(DATA_PATH+"mnist2500_X.txt")
     labels = np.loadtxt(DATA_PATH+"mnist2500_labels.txt")
-    Y = sne(X, 2, 50, 20.0)
-    pylab.scatter(Y[:, 0], Y[:, 1], 5, labels)
-    pylab.savefig(SAVE_PATH+"tsne.jpg")
+    Y, P, Q, err_list = sne(X, labels, 2, 50, 20.0)
+    plot_result(Y)
+    iter_list = list(range(int(max_iter/10)))
+    plot_similarity(P, Q)
+    # pylab.scatter(Y[:, 0], Y[:, 1], 5, labels)
+    # pylab.savefig(f"{SAVE_PATH}{method}_{dim_reduction}.jpg")
+    # pylab.clf()
+    # pylab.cla()
+    # pylab.close()
+    # pylab.plot(iter_list, err_list)
+    # pylab.savefig(f"{SAVE_PATH}{method}_{dim_reduction}_error.jpg")
     # pylab.show()
